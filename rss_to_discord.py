08ba739540126910da7fabdef40d2444310f6b71
Kopasz98 <kozmabalazs9813@gmail.com>
2025-12-17 10:08:39 +0100

    Update rss_to_discord.py

--- rss_to_discord.py
+++ rss_to_discord.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import feedparser
 import requests
 import json
@@ -7,76 +8,76 @@ import hashlib
 WEBHOOK_URL = os.environ["DISCORD_WEBHOOK"]
 
 RSS_FEEDS = [
-"https://www.dexerto.com/feed/",
-"https://dotesports.com/feed",
-"https://streamerbans.com/rss",
-"https://www.theverge.com/creators/rss/index.xml",
-"https://www.polygon.com/rss/index.xml",
-"https://insider-gaming.com/feed/",
-"https://gamerant.com/feed/",
-"https://www.pcgamer.com/rss/",
-"https://kotaku.com/rss",
-"https://www.gamesradar.com/rss/",
+    "https://www.dexerto.com/feed/",
+	"https://dotesports.com/feed",
+	"https://streamerbans.com/rss",
+	"https://www.theverge.com/creators/rss/index.xml",
+	"https://www.polygon.com/rss/index.xml",
+	"https://insider-gaming.com/feed/",
+	"https://gamerant.com/feed/",
+	"https://www.pcgamer.com/rss/",
+	"https://kotaku.com/rss",
+	"https://www.gamesradar.com/rss/",
 
-"https://twitchtracker.com/blog/rss",
-"https://sullygnome.com/articles/rss",
-"https://www.twitchmetrics.net/blog/rss",
-"https://streamernews.live/rss",
-"https://blog.twitch.tv/en/feed/",
-"https://www.streamscheme.com/feed/",
-"https://nerdordie.com/blog/feed/",
-"https://www.own3d.tv/blog/feed/",
-"https://blog.streamlabs.com/rss",
-"https://golightstream.com/blog/rss",
+	"https://twitchtracker.com/blog/rss",
+	"https://sullygnome.com/articles/rss",
+	"https://www.twitchmetrics.net/blog/rss",
+	"https://streamernews.live/rss",
+	"https://blog.twitch.tv/en/feed/",
+	"https://www.streamscheme.com/feed/",
+	"https://nerdordie.com/blog/feed/",
+	"https://www.own3d.tv/blog/feed/",
+	"https://blog.streamlabs.com/rss",
+	"https://golightstream.com/blog/rss",
 
-"https://www.tubefilter.com/feed/",
-"https://socialblade.com/blog/feed",
-"https://blog.youtube/rss.xml",
-"https://www.youtube.com/creatorinsider/rss",
-"https://vidiq.com/blog/rss/",
-"https://blog.hootsuite.com/feed/",
-"https://later.com/blog/rss/",
-"https://influencermarketinghub.com/feed/",
+	"https://www.tubefilter.com/feed/",
+	"https://socialblade.com/blog/feed",
+	"https://blog.youtube/rss.xml",
+	"https://www.youtube.com/creatorinsider/rss",
+	"https://vidiq.com/blog/rss/",
+	"https://blog.hootsuite.com/feed/",
+	"https://later.com/blog/rss/",
+	"https://influencermarketinghub.com/feed/",
 
-"https://blog.kick.com/rss",
-"https://streamernews.live/rss",
-"https://www.dexerto.com/feed/",
-"https://dotesports.com/feed",
-"https://afkgaming.com/rss",
+	"https://blog.kick.com/rss",
+	"https://streamernews.live/rss",
+	"https://www.dexerto.com/feed/",
+	"https://dotesports.com/feed",
+	"https://afkgaming.com/rss",
 
-"https://esportsinsider.com/feed",
-"https://www.esports.net/feed/",
-"https://upcomer.com/feed/",
-"https://ggrecon.com/feed/",
-"https://www.theloadout.com/feed",
-"https://win.gg/feed/",
-"https://siege.gg/rss",
+	"https://esportsinsider.com/feed",
+	"https://www.esports.net/feed/",
+	"https://upcomer.com/feed/",
+	"https://ggrecon.com/feed/",
+	"https://www.theloadout.com/feed",
+	"https://win.gg/feed/",
+	"https://siege.gg/rss",
 
-"https://passionfru.it/feed/",
-"https://thepublishpress.com/feed/",
-"https://signalfire.com/blog/feed/",
-"https://www.morningbrew.com/creator-economy/rss",
-"https://influencermarketinghub.com/feed/",
-"https://www.businessinsider.com/rss",
-"https://www.forbes.com/creators/feed/",
+	"https://passionfru.it/feed/",
+	"https://thepublishpress.com/feed/",
+	"https://signalfire.com/blog/feed/",
+	"https://www.morningbrew.com/creator-economy/rss",
+	"https://influencermarketinghub.com/feed/",
+	"https://www.businessinsider.com/rss",
+	"https://www.forbes.com/creators/feed/",
 
-"https://blog.twitch.tv/en/feed/",
-"https://blog.youtube/rss.xml",
-"https://blog.kick.com/rss",
-"https://obsproject.com/blog/rss",
-"https://blog.streamlabs.com/rss",
-"https://discord.com/blog/rss",
+	"https://blog.twitch.tv/en/feed/",
+	"https://blog.youtube/rss.xml",
+	"https://blog.kick.com/rss",
+	"https://obsproject.com/blog/rss",
+	"https://blog.streamlabs.com/rss",
+	"https://discord.com/blog/rss",
 
-"https://esport1.hu/rss",
-"https://www.gamestar.hu/rss",
-"https://www.pcguru.hu/rss",
-"https://hu.ign.com/rss",
-"https://refresher.hu/rss",
+	"https://esport1.hu/rss",
+	"https://www.gamestar.hu/rss",
+	"https://www.pcguru.hu/rss",
+	"https://hu.ign.com/rss",
+	"https://refresher.hu/rss"
 
 ]
 
 INCLUDE_KEYWORDS = [k.lower() for k in [
-  "Twitch","Youtube","Kik",
+    "Twitch","Youtube","Kik"
 ]]
 
 EXCLUDE_KEYWORDS = [k.lower() for k in [
@@ -84,7 +85,7 @@ EXCLUDE_KEYWORDS = [k.lower() for k in [
     "giveaway", "free skins",
     "sale", "discount",
     "top 10", "best of",
-    "opinion", "editorial","ban","esport",
+    "opinion", "editorial","ban","esport","YouTuber"
 ]]
 
 POSTED_FILE = "posted.json"
@@ -118,7 +119,6 @@ if os.path.exists(POSTED_FILE):
             posted = set(json.load(f))
     except Exception:
         posted = set()
-
 
 new_posts = []
 
